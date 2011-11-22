d56167e37e97f74412e802fd0a5f2c6b903650b8
Andrei-Adnan Ismail <iandrei@gmail.com>
2011-11-22 19:24:17 +0200

    Simple application that writes webcam frames to a database. (Closes #4)

---
+++ src/WebcamTest.java
@@ -0,0 +1,39 @@
+import me.prettyprint.cassandra.serializers.BytesArraySerializer;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.serializers.TimeUUIDSerializer;
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
+import me.prettyprint.hector.api.factory.HFactory;
+import me.prettyprint.hector.api.mutation.Mutator;
+
+import com.googlecode.javacv.*;
+import static com.googlecode.javacv.cpp.opencv_core.*;
+
+public class WebcamTest {
+	public static void main(String[] args) throws Exception {
+		Cluster cluster = HFactory.getOrCreateCluster("Test Cluster", "localhost:9160");
+		Keyspace keyspace = HFactory.createKeyspace("v1", cluster);
+        CanvasFrame frame = new CanvasFrame("Some Title");
+
+        FrameGrabber grabber = new OpenCVFrameGrabber(0);
+        grabber.setImageWidth(640);
+        grabber.setImageWidth(480);
+        grabber.setBitsPerPixel(16);
+        grabber.start();
+
+        IplImage grabbedImage = grabber.grab();   
+        while (frame.isVisible() && (grabbedImage = grabber.grab()) != null) {
+        	grabbedImage = grabber.grab();
+            frame.showImage(grabbedImage);
+            Mutator<com.eaio.uuid.UUID> mutator = HFactory.createMutator(keyspace, TimeUUIDSerializer.get());
+            byte[] rawImg = new byte[grabbedImage.getByteBuffer().capacity()];
+            grabbedImage.getByteBuffer().get(rawImg);
+    		mutator.insert(new com.eaio.uuid.UUID(), "WebcamFrames", HFactory.createColumn("Frame", rawImg, StringSerializer.get(), BytesArraySerializer.get()));
+    		mutator.execute();
+        }
+        
+        grabber.stop();
+        frame.dispose();
+    }
+}
