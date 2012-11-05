9d3efd89d98c9f43003339b9eaeeb7c96e3d4aaf
AmI Lab <iandrei+amilab@gmail.com>
2012-11-05 09:02:50 +0200

    Added python script that reads from kestrel and saves skeleton measurements to Mongo.

---
+++ acquisition/mongo_writer.py
@@ -0,0 +1,45 @@
+import pymongo
+import kestrel
+import json
+
+QUEUE = 'measurements'
+KESTREL_SERVERS = ['127.0.0.1:22133']
+DB_NAME = 'measurements'
+COLLECTION_NAME = 'docs'
+
+kestrel_connection = kestrel.Client(KESTREL_SERVERS)
+mongo_connection = pymongo.Connection()
+db = getattr(mongo_connection, DB_NAME)
+collection = getattr(db, COLLECTION_NAME)
+
+while True:
+	try:
+		# Step 1 - get message from kestrel queue
+		message = kestrel_connection.get(QUEUE, timeout = 1)
+		if not message:
+			print "Could not get message. Retrying ..."
+			continue
+		
+		# Step 2 - try to decode it assuming it's in correct
+		# JSON format
+		try:
+			doc = json.loads(message)
+		except:
+			print "Did not get valid JSON from queue %s" % QUEUE
+			print "Message = %s" % message
+			import traceback
+			traceback.print_exc()
+			continue
+
+		# Step 3 - try to save to mongo
+		try:
+			collection.save(doc)
+		except:
+			print "Could not save document to mongo"
+			print "doc = %r" % doc
+			import traceback
+			traceback.print_exc()
+	except:
+		print "Error while getting message from queue %s" % QUEUE
+		import traceback
+		traceback.print_exc()
