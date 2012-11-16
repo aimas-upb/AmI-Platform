704de39373a449273276e7a5419b7642bc78809a
Andrei Ismail <iandrei@gmail.com>
2012-11-16 16:01:11 +0200

    Added router module that directs incoming messages from sensors towards processing pipelines. (#14)

---
+++ pipeline/router.py
@@ -0,0 +1,18 @@
+import pymongo
+
+from core import PDU
+from core import constants
+
+class Router(PDU):
+    """ PDU that routes incoming measurements from sensors to the
+        actual processing pipelines """
+
+    QUEUE = 'measurements'
+
+    def process_message(self, message):
+        # Route messages towards mongo-writer
+        self.send_to('mongo-writer', message)
+
+if __name__ == "__main__":
+    module = MongoWriter()
+    module.run()
