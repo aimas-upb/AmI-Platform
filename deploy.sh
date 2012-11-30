9e137c83b5ee03243c8748a6849a8c1457709978
AmI Lab <iandrei+amilab@gmail.com>
2012-11-30 15:57:20 +0200

    deploy script will not pull from repository

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 # Redeploy everything script
 
-branch=$(git rev-parse --abbrev-ref HEAD)
+# branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Get rid of non-committed changes
-git reset --hard HEAD
+# git reset --hard HEAD
 
 # Get latest changes from repo
-git pull origin $branch
+# git pull origin $branch
 
 # Make sure that filesystem is in sync with git index
-git reset --hard HEAD
+# git reset --hard HEAD
 
 # Read the list of services, and copy the new upstart & monit files + restart
 while read -r service_name; do
