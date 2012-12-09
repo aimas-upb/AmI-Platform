04941cb4dff69fe0ef9425fd5bbda2d5ded03f5c
AmI Lab <iandrei+amilab@gmail.com>
2012-12-09 16:12:41 +0200

    Simple Bottle-based API that returns the latest RGB image from kinect DAQ. (#4)

---
+++ api/api.py
@@ -0,0 +1,15 @@
+import json
+import bottle
+from bottle import route, run, request, abort
+from pymongo import Connection
+from core import settings
+
+@route('/latest_kinect_rgb', method='GET')
+def get_latest_kinect_rgb():
+    connection = Connection(settings.MONGO_SERVER)
+    db = connection.measurements.docs
+    image = db.find({'sensor_type': 'kinect_rgb'}, limit = 1).sort('created_at', -1)[0]
+    del image['_id']
+    return json.dumps(image)
+
+run(host='127.0.0.1', port=8000)
