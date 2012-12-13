cdf384b4cf411e0fdd7c29d1490c8621a02dec38
AmI Lab <iandrei+amilab@gmail.com>
2012-12-13 10:18:53 +0200

    Enabling CORS for ami-crunch-01.local host.

--- dashboard/conf/general.user.js
+++ dashboard/conf/general.user.js
@@ -1,8 +1,8 @@
 var App = App || {};
 
 App.user = {
-    FRONTAPI_URL: 'http://127.0.0.1',
-    FRONTEND_URL: 'http://127.0.0.1',
-    STATIC_URL: 'http://127.0.0.1',
+    FRONTAPI_URL: 'http://ami-crunch-01.local',
+    FRONTEND_URL: 'http://ami-crunch-01.local',
+    STATIC_URL: 'http://ami-crunch-01.local',
     LOG_LEVEL: 1
 };
