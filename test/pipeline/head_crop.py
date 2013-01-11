38b86c02a3cdbf36cbf4e916b1c20265960aca14
AmI Lab <iandrei+amilab@gmail.com>
2013-01-11 11:44:33 +0200

    Updating tests for head_crop. (#73)

--- test/pipeline/head_crop.py
+++ test/pipeline/head_crop.py
@@ -1,8 +1,10 @@
+import base64
 import time
 from unittest import TestCase
 
 from mock import MagicMock, patch
 from nose.tools import ok_, eq_
+from PIL import Image
 
 from pipeline.head_crop import HeadCrop
 
@@ -25,24 +27,24 @@ class HeadCropTest(TestCase):
     @patch.object(HeadCrop, 'send_to')
     # Because we don't have image + skeleton, face detection should
     # be called, and make sure it doesn't return anything useful
-    @patch.object(HeadCrop, 'face_detect', return_value = None)
-    @patch.object(HeadCrop, 'crop_head', return_value = None)
-    def test_sole_image_without_face_doesnt_sent_to_recognition(self, crop_head, face_detect, send_to):
+    @patch.object(HeadCrop, '_crop_head_using_face_detection', return_value = None)
+    @patch.object(HeadCrop, '_crop_head_using_skeleton', return_value = None)
+    def test_sole_image_without_face_doesnt_sent_to_recognition(self, skeleton, face_detect, send_to):
         pdu = HeadCrop()
         pdu.process_message(self._image_message())
         face_detect.assert_called_once_with()
-        eq_(crop_head.call_count, 0, "crop_head should only be called if it has "
+        eq_(skeleton.call_count, 0, "crop_head should only be called if it has "
                                    "a corresponding image and skeleton")
         eq_(send_to.call_count, 0, "send_to should only be called if it has "
                                    "a corresponding image and skeleton")
         
     @patch.object(HeadCrop, 'send_to')
-    @patch.object(HeadCrop, 'face_detect', return_value = None)
-    @patch.object(HeadCrop, 'crop_head', return_value = None)
-    def test_sole_skeleton_doesnt_send_to_recognition(self, crop_head, face_detect, send_to):
+    @patch.object(HeadCrop, '_crop_head_using_face_detection', return_value = None)
+    @patch.object(HeadCrop, '_crop_head_using_skeleton', return_value = None)
+    def test_sole_skeleton_doesnt_send_to_recognition(self, skeleton, face_detect, send_to):
         pdu = HeadCrop()
         pdu.process_message(self._skeleton_message())
-        eq_(crop_head.call_count, 0, "crop_head should only be called if it has "
+        eq_(skeleton.call_count, 0, "crop_head should only be called if it has "
                                    "a corresponding image and skeleton")
         eq_(send_to.call_count, 0, "send_to should only be called if it has "
                                    "a corresponding image and skeleton")
@@ -51,30 +53,27 @@ class HeadCropTest(TestCase):
         
         
     @patch.object(HeadCrop, 'send_to')
-    @patch.object(HeadCrop, 'face_detect', return_value = None)
-    @patch.object(HeadCrop, 'crop_head', return_value = None)
-    def test_one_image_and_one_skeleton_not_close_enough(self, crop_head, face_detect, send_to):
+    @patch.object(HeadCrop, '_crop_head_using_face_detection', return_value = None)
+    @patch.object(HeadCrop, '_crop_head_using_skeleton', return_value = None)
+    def test_one_image_and_one_skeleton_not_close_enough(self, skeleton, face_detect, send_to):
         pdu = HeadCrop()
         pdu.process_message(self._skeleton_message())
         time.sleep(pdu.MAX_TIME)
         pdu.process_message(self._image_message())
         face_detect.assert_called_once_with()
-        eq_(crop_head.call_count, 0, "crop_head should only be called if it has "
+        eq_(skeleton.call_count, 0, "crop_head should only be called if it has "
                                    "a corresponding image and skeleton")
         eq_(send_to.call_count, 0, "send_to should only be called if it has "
                                    "a corresponding image and skeleton")
        
     @patch.object(HeadCrop, 'send_to')
-    @patch.object(HeadCrop, 'face_detect', return_value = None)
-    @patch.object(HeadCrop, 'crop_head', return_value = {})
-    def test_one_image_and_one_skeleton_close_enough(self, crop_head, face_detect, send_to):
+    @patch.object(HeadCrop, '_crop_head_using_face_detection', return_value = None)
+    @patch.object(HeadCrop, '_crop_head_using_skeleton', return_value = Image.frombuffer('RGB', (1, 1), base64.b64decode('AAAA')))
+    def test_one_image_and_one_skeleton_close_enough(self, skeleton, face_detect, send_to):
         pdu = HeadCrop()
         pdu.process_message(self._skeleton_message())
         pdu.process_message(self._image_message())
         eq_(face_detect.call_count, 0, "face_detect should only be called if it has "
                                    "a corresponding image and skeleton")
-        send_to.assert_called_once_with('face-recognition', {})
-        crop_head.assert_called_once_with()
-        
-    
-    
+        send_to.assert_called_once_with('face-recognition', {'width': 1, 'image': 'AAAA', 'height': 1})
+        skeleton.assert_called_once_with()
