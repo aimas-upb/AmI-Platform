8ac5f2c729f933bbdcc1f1beeef659440d4ea915
AmI Lab <iandrei+amilab@gmail.com>
2013-01-11 15:51:11 +0200

    Fixed syntax bugs. (#73)

--- lib/opencv.py
+++ lib/opencv.py
@@ -10,12 +10,13 @@ def crop_face_from_image(image):
     faces = face_detect(image)
     if len(faces) == 0:
         return None
-    (x, y, w, h) = faces[0]
+    
+    (x, y, w, h) = faces[0][0]
     
     # Take the first detected face and scale the rectangle
     # 2 times around its center.
     face_rect = (x, y, x + w, y + h)
-    face_rect = rectangle_scale(face_rect, 2)
+    face_rect = rectangle_scale(*face_rect, factor = 2)
     
     # Intersect this with the image rectangle
     image_rect = (0, 0, image.size[0], image.size[1])
