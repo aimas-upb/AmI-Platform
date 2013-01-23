dd92fd49b9d52bb387207c0f3f2ba67dd3d9657d
Lacea Alin <alin.lacea@aza.ro>
2013-01-23 18:57:27 +0200

    Added Chef-Solo installation script.

---
+++ chef/install_chef-solo.sh
@@ -0,0 +1,28 @@
+#Add Opscode repository
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
+
+#Add GPG Key
+sudo mkdir -p /etc/apt/trusted.gpg.d
+gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
+
+#Update all packages
+sudo apt-get update -y
+sudo apt-get install opscode-keyring -y # permanent upgradeable keyring
+sudo apt-get upgrade -y
+
+
+#Install chef
+sudo apt-get install chef
+#Install ruby & dependencies.
+sudo apt-get install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl -y
+
+#Install ruby gems
+cd /tmp
+curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
+tar zxf rubygems-1.8.10.tgz
+cd rubygems-1.8.10
+sudo ruby setup.rb --no-format-executable
+
+#Install Chef Gem
+sudo gem install chef --no-ri --no-rdoc
