beefc71cf7ae7c35dce2567669666e9eea5597b9
Andrei Ismail <iandrei@gmail.com>
2013-02-01 15:55:58 +0200

    Added empty implementation for measurements player. (#97)

---
+++ core/measurements_player.py
@@ -0,0 +1,12 @@
+
+class MeasurementsPlayer(object):
+	def __init__(self, data_file, callback):
+		self.data_file = data_file
+		self.callback = callback
+
+	def play(self):
+		pass
+
+# if __name__ == "__main__":
+#     module = MeasurementsPlayer(data_file, callback)
+#     module.run()
