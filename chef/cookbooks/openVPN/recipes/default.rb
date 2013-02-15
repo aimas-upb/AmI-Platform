b422c58dc85f17aa4467fc4c02282ea1b3e5f69d
Alin Lacea <alin.lacea@aza.ro>
2013-02-15 16:56:40 +0200

    OpenVPN cookbook.

---
+++ chef/cookbooks/openVPN/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name: :kinect
+# Recipe: :default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "openvpn" do
+	action :install
+end
+
+directory "keys" do
+	action :create
+	owner "root"
+	path "/etc/openvpn/keys"
+end
+
+remote_file "conf_file" do
+	action :create
+	path "/etc/openvpn/client.conf"
+	owner root
+	source node.openvpn.config_file_url
+end
