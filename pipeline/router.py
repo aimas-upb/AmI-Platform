d7fceda6412c7eb095f53d425b127fefc864adde
Andrei Ismail <iandrei@gmail.com>
2013-02-22 19:29:01 +0200

    Giving up on recorder for now, it can't run fast enough.

--- pipeline/router.py
+++ pipeline/router.py
@@ -18,7 +18,7 @@ class Router(PDU):
 
         self.send_to('mongo-writer', message)
         self.send_to('head-crop', message)
-        self.send_to('recorder', message)
+        # self.send_to('recorder', message)
         self.send_to('room-position', message)
 
 if __name__ == "__main__":
