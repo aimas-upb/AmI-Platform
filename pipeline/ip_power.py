4d28455c6f095ed4dd6f3d8fd67d7b9e269b458d
Andrei Ismail <iandrei@gmail.com>
2013-02-22 12:07:34 +0200

    Adding logging to IP power. (#119)

--- pipeline/ip_power.py
+++ pipeline/ip_power.py
@@ -19,21 +19,27 @@ class IPPower(PDU):
 
         """ Prepare parameters for HTTP request """
         params = copy.deepcopy(message)
-        # cmd -> state conversion
-        cmd = params['cmd'].lower()
-        params['state'] = {'on': 0, 'off': 1}[cmd]
-        del params['cmd']
+
+        # Convert command to state value
+        params['state'] = {'on': 0, 'off': 1}[params.pop('cmd').lower()]
+
         # Fill in default user & password if they aren't provided
         params['user'] = params.get('user', self.DEFAULT_USER)
         params['password'] = params.get('password', self.DEFAULT_PASSWORD)
+        self.logger.info("Will perform HTTP request using params %r" % params)
 
         url = "http://%(ip)s/Set.cmd?CMD=SetPower+P6%(output)s=%(state)s" %\
                 params
-        r = requests.get(url, auth=(params['user'], params['password']))
+        self.logger.info("Performing HTTP request to URL %s" % url)
 
+        # Make the actual request and log the result
+        r = requests.get(url, auth=(params['user'], params['password']))
         if (r.status_code != 200):
-            self.log("HTTP request failed with status code %d and response " %
-                     r.status_code, r.text)
+            self.logger.info("HTTP request failed with status code %d "
+                             "and response " % r.status_code, r.text)
+        else:
+            self.logger.info("Everything went allright in communication with "
+                             "IP power equipment")
 
     def validate_message(self, message):
         for required_key in ['cmd', 'output', 'ip']:
