141ef67c5e8eff5e9044d01615a92f1d39b824b4
Andrei Ismail <iandrei@gmail.com>
2013-02-22 18:01:37 +0200

    Even prettier logging.

--- lib/log.py
+++ lib/log.py
@@ -4,4 +4,4 @@ import logging
 def setup_logging():
     """ Sets up logging to standard output for a PDU """
     logging.basicConfig(level=logging.INFO,
-                        format="%(asctime)s - %(process)d^%(thread)d %(filename)s:%(funcName)s:%(lineno)d - %(message)s")
+                        format="[%(asctime)s] - [%(process)d * %(thread)d] %(filename)s:%(funcName)s:%(lineno)d - %(message)s")
