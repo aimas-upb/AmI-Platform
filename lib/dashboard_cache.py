e5239c1e0248ec75894dbf41412874cb93c03ede
ami <ami@ami-crunch-06.(none)>
2013-02-23 16:51:06 +0200

    Logging Redis operations.

--- lib/dashboard_cache.py
+++ lib/dashboard_cache.py
@@ -1,6 +1,10 @@
+import logging
+
 import redis
 
 from core import settings
+
+logger = logging.getLogger(__name__)
 
 class DashboardCache(object):
     """ Cache which stores and retrieves images for dashboard usage. """
@@ -12,10 +16,12 @@ class DashboardCache(object):
 
     def put(self, sensor_id, sensor_type, measurement_type, measurement):
         key = self._redis_key(sensor_id, sensor_type, measurement_type)
+        logger.info("Putting in redis at key %s" % key)
         self.redis_cache.set(key, measurement)
 
     def get(self, sensor_id, sensor_type, measurement_type):
         key = self._redis_key(sensor_id, sensor_type, measurement_type)
+        logger.info("Getting from redis at key %s" % key)
         return self.redis_cache.get(key)
 
     def _redis_key(self, sensor_id, sensor_type, measurement_type):
