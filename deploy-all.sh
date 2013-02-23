a0bbfcf915cb255205d0dd4790c5dc7ac2ded277
Andrei Ismail <iandrei@gmail.com>
2013-02-23 12:16:13 +0200

    ami-crunch-03 out of the loop.

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,4 +1,4 @@
-for server in ami-daq-01 ami-daq-02 ami-daq-03 ami-daq-04 ami-daq-05 ami-crunch-02 ami-crunch-03 ami-crunch-04 ami-crunch-05 ami-crunch-06
+for server in ami-daq-01 ami-daq-02 ami-daq-03 ami-daq-04 ami-daq-05 ami-crunch-02 ami-crunch-04 ami-crunch-05 ami-crunch-06
 do
     echo "Redeploying $server..."
     ssh root@$server.local 'su -c "cd ~/AmI-Platform; ./deploy.sh --fresh" ami'
