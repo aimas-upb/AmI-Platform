356d4abbe7c2b90b96738679fcf98ea06455ad23
Andrei Ismail <iandrei@gmail.com>
2013-02-23 09:17:38 +0200

    Dashboard is running on ami-crunch-06.

--- dashboard/conf/general.user.js
+++ dashboard/conf/general.user.js
@@ -1,8 +1,8 @@
 var App = App || {};
 
 App.user = {
-    FRONTAPI_URL: 'http://ami-crunch-01.local',
-    FRONTEND_URL: 'http://ami-crunch-01.local',
-    STATIC_URL: 'http://ami-crunch-01.local',
+    FRONTAPI_URL: 'http://ami-crunch-06.local',
+    FRONTEND_URL: 'http://ami-crunch-06.local',
+    STATIC_URL: 'http://ami-crunch-06.local',
     LOG_LEVEL: 1
 };
