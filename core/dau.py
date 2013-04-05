c1ddea206bfe3a8e2a96834cb65a5fface8faea4
Alin Lacea <alin.lacea@aza.ro>
2013-04-05 18:03:17 +0300

    Fixed typos and indentation issues in DAU Class.

--- core/dau.py
+++ core/dau.py
@@ -3,6 +3,7 @@ import logging
 import pymongo
 import kestrel
 import settings
+import time
 """
 	Data Acquisition Unit for Ami-Lab framework
 	Its architecture resembles the design of PDU Class.
@@ -26,37 +27,37 @@ class DAU(object):
 		
 	@property
 	def output_queue_system(self):
-		return self.output_queue
+		return self._output_queue
 		
-    def send_to(self, queue, message):
-        """ Send a message to another queue. """
-        self.log("Enqueueing message to %s" % queue, level = logging.INFO)
-        self.output_queue_system.add(queue, json.dumps(message))
+	def send_to(self, queue, message):
+		""" Send a message to another queue. """
+		self.log("Enqueueing message to %s" % queue, level = logging.INFO)
+		self.output_queue_system.add(queue, json.dumps(message))
 
-    def _start_running(self):
-        """ Mark the current PDU as running. """
-        self._running = True
+	def _start_running(self):
+		""" Mark the current PDU as running. """
+		self._running = True
 
-    def _stop_running(self):
-        """ Mark the current PDU as NOT running. """
-        self._running = False
+	def _stop_running(self):
+		""" Mark the current PDU as NOT running. """
+		self._running = False
 
-    def _is_running(self):
-        """ Query whether the current PDU is running or not. """
-        result = self._running
-        return result
+	def _is_running(self):
+		""" Query whether the current PDU is running or not. """
+		result = self._running
+		return result
 
-		
+
 	def acquire_data(self):
 		#It should be designed to return a list of messages, as it is possible to retrieve multiple datasets at once
 		raise NotImplemented("Please implement this in your sub-class!")
-		
+
 	def run(self):
 		self._start_running()
 		self.log("DAU %s is alive!" %self.__class__.__name__)
-		
+
 		while self._is_running():
 			messages = self.acquire_data()
 			for msg in messages:
-				self.send_to(QUEUE, message)
-			time.sleep(DATA_SAMPLING_FREQUENCY /1000f)
+				self.send_to(self.QUEUE, msg)
+			time.sleep(self.DATA_SAMPLING_FREQUENCY /1000.0)
