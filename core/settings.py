36123497923d6a204d50cada25d1919a22fa65da
Cosmin Marian <cosmin1611@gmail.com>
2013-07-12 11:41:14 +0300

    added '.local' sufix to server addresses in settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,6 @@
-KESTREL_SERVERS=['ami-crunch-01:22133']
-MONGO_SERVER='amilab-daq-10:27017'
-REDIS_SERVER='amilab-daq-10'
+KESTREL_SERVERS=['ami-crunch-01.local:22133']
+MONGO_SERVER='amilab-daq-10.local:27017'
+REDIS_SERVER='amilab-daq-10.local'
 REDIS_PORT=6379
 REDIS_DASHBOARD_DB=0
 REDIS_SESSION_DB=1
