82820731e420d6460ed4efd8609378a630fe418c
Andrei Ismail <iandrei@gmail.com>
2013-08-16 14:54:34 +0300

    Added manifest for basic machine bootstrap. (#231)
    This is used as an intermediate step until the code is pulled from the
    repo and thus the real manifest can be ran.

---
+++ provisioning/bootstrap.pp
@@ -0,0 +1,16 @@
+user { "ami":
+    ensure     => present,
+    managehome => true,
+}
+
+package { "git":
+    ensure => present,
+}
+
+vcsrepo { "/home/ami/AmI-Platform":
+    ensure   => present,
+    provider => git,
+    source   => "git@github.com:ami-lab/AmI-Platform.git",
+    user     => "ami",
+    require  => Package["git"],
+}
