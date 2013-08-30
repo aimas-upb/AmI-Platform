74d0e82c2dda1d966cb623bddc1fd13e35cf333d
Diana Tatu <diana.tatu@gmail.com>
2013-08-30 18:22:15 +0300

    Added pipeline tests for each dump file. #222

---
+++ test/pipeline_tests/test_crop_diana3.py
@@ -0,0 +1,31 @@
+import logging
+
+from nose.plugins.attrib import attr
+
+from core import PipelineTest
+from core.constants import PROJECT_PATH
+from pipeline.router import Router
+from pipeline.head_crop import HeadCrop
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class TestCrop(PipelineTest):
+    """#110 - Test that I am in front of the camera and it crops my head."""
+
+    PDUs = [Router, HeadCrop]
+    DATA_FILE = '%s/dumps/diana3.txt' % PROJECT_PATH
+    NB_MIN_EXPECTED_FACES = 11
+
+    @attr('pipeline', 'slow')
+    def test_that_pipeline_test_works_ok(self):
+        self._test_pipeline()
+
+    def check_results(self):
+        logger.info("Still %s alive pdu threads", self.alive_pdus())
+        nb_faces = 0
+        while (self._queue_system.get('face-recognition', 1) is not None):
+            nb_faces = nb_faces + 1
+
+        self.assertGreaterEqual(nb_faces, self.NB_MIN_EXPECTED_FACES)
