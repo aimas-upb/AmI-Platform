de2c704a46e446a1135dfad4c3dec6486d4468de
Diana Tatu <diana.tatu@gmail.com>
2013-08-30 12:24:48 +0300

    Display the right sleeping time for the measurements player. #222

--- core/measurements_player.py
+++ core/measurements_player.py
@@ -4,7 +4,7 @@ import time
 
 logger = logging.getLogger(__name__)
 
-from experiment_file import ExperimentFile
+from core.experiment_file import ExperimentFile
 
 
 class MeasurementsPlayer(object):
@@ -24,7 +24,7 @@ class MeasurementsPlayer(object):
             if not measurement_start:
                 measurement_start = measurement['created_at']
 
-            logger.info('Sending a message %s' % measurement['sensor_id'])
+            logger.info('Sending a message %s', measurement['sensor_id'])
             nb_messages += 1
             playback_delta = int(time.time()) - playback_start
             measurement_delta = measurement['created_at'] - measurement_start
@@ -32,17 +32,12 @@ class MeasurementsPlayer(object):
             if playback_delta < measurement_delta:
                 # Sleep in order to wait for the correct time to play
                 # back this measurement.
-                logger.info('sleeping %d' % (measurement_delta - playback_delta))
-                time.sleep((measurement_delta - playback_delta) / 1000000.0)
-            """
-                else:
-                # Skip this measurement because we're behind
-                continue
-            """
+                sleep_time = (measurement_delta - playback_delta) / 1000000.0
+                logger.info('sleeping %r', sleep_time)
+                time.sleep(sleep_time)
 
             self.callback(json.dumps(measurement))
 
         logger.info('Done replaying file with %s messages', nb_messages)
 
         self.experiment_file.close()
-
