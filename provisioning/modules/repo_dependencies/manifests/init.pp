a3c485fe7553148652a5407bfa8080f6ae5b5ce1
Andrei Ismail <iandrei@gmail.com>
2013-08-31 19:17:12 +0300

    OpenCV is now poart of daq_dependencies. (#231)

--- provisioning/modules/repo_dependencies/manifests/init.pp
+++ provisioning/modules/repo_dependencies/manifests/init.pp
@@ -1,7 +1,6 @@
 class repo_dependencies {
 
     class { "repo_dependencies::python_dependencies": }
-    class { "repo_dependencies::opencv": }
     class { "repo_dependencies::daq_dependencies": }
 
 }
