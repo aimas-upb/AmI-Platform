c16a847cf91dc8e6f3c52b1052293eeed643ba49
Andrei Ismail <iandrei@gmail.com>
2013-09-07 17:45:24 +0300

    Fixing brainfucked syntax errors for code_settings module. (#255)

--- provisioning/modules/code_settings/manifests/init.pp
+++ provisioning/modules/code_settings/manifests/init.pp
@@ -7,18 +7,18 @@ class code_settings($redis_hostname,
 
     file {'/home/ami/AmI-Platform/dashboard/conf/general.user.js':
         ensure	=> present,
-        content => template('code_settings/general_user_js.erb')
+        content => template('code_settings/general_user_js.erb'),
         owner   => 'ami',
         group	=> 'ami',
-        mode	=> '0644',
+        mode	=> '0644'
     }
 
     file {'/home/ami/AmI-Platform/core/settings_local.py':
         ensure	=> present,
-        content => template('code_settings/settings_local_py.erb')
+        content => template('code_settings/settings_local_py.erb'),
         owner   => 'ami',
         group	=> 'ami',
-        mode	=> '0644',
+        mode	=> '0644'
     }
 
 }
