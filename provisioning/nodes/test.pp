15116139eff2814bbee81e9d5a6b6154d566e9ab
Diana Tatu <diana.tatu@gmail.com>
2013-09-13 19:06:10 +0300

    Add crunch.pp to test manifest. #222

--- provisioning/nodes/test.pp
+++ provisioning/nodes/test.pp
@@ -1,9 +1,6 @@
 class test {
     class {"kestrel": }
-    
     class { "session_store": }
-    # Dashboard API should be close to the Redis it's reading from
-    class { "dashboard_api": }
-    
     class { "measurement_store": }
+    class { "cruncher": }
 }
