01bd9f1a9fe6a2bd0079c96952cff11d4c51cdf2
Diana Tatu <diana.tatu@gmail.com>
2013-09-27 13:47:30 +0300

    Fix "Syntax error at 'if'; expected '}'". #222

--- provisioning/modules/kestrel/manifests/init.pp
+++ provisioning/modules/kestrel/manifests/init.pp
@@ -2,6 +2,17 @@ class kestrel ($max_ram='1024m') {
 
     include wget
 
+    # I am considering this an ugly HACK :)
+    # We should treat the "Duplicate declaration of Package[unzip]" error
+    # in a different way.
+    # https://groups.google.com/forum/?fromgroups=#!topic/puppet-users/julAujaVsVk
+    # TODO(diana): Fix this after reading the whole Puppet documentation.
+    if ! defined (Package["unzip"]) {
+        package { 'unzip':
+            ensure => present,
+        }
+    }
+
     # Kestrel needs java
     package {"openjdk-7-jre-headless":
         ensure => present
@@ -12,13 +23,6 @@ class kestrel ($max_ram='1024m') {
     wget::fetch { "download_kestrel_tarball":
         source  => "http://robey.github.io/kestrel/download/kestrel-2.4.1.zip",
         destination => "/usr/local/kestrel-2.4.1.zip"
-    }
-
-    ->
-
-    if ! defined ("unzip") {
-    package { 'unzip':
-        ensure => present,
     }
 
     ->
