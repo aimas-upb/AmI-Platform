a32b2f3a5200d3f82ea59aeae069f1c08d49fdd6
cosmin-marian <cosmin>
2013-10-18 18:00:12 +0300

    Cherry pick: The application can now read command line parameters that specify the kinect and use them. Work in progress to make two separate services run on the same machine

---
+++ acquisition/kinect/kinect-acq-wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+KINECT_SERIAL=$AMI_SENSOR_KINECT_SERIAL
+if [ -n $KINECT_SERIAL  ]
+then
+    parts=(`lsusb -v -d 045e:02ae | grep -e "Bus\|iSerial" | grep -B 1 $KINECT_SERIAL | head -n1 | cut -d' ' -f2,4 | cut -d':' -f1`)
+    bus=$(echo ${parts[0]} | sed 's/^0*//')
+    device=$(echo ${parts[1]} | sed 's/^0*//')
+    `dirname $0`/Bin/x64-Release/Sample-NiUserTracker $KINECT_SERIAL $bus $device
+else
+    `dirname $0`/Bin/x64-Release/Sample-NiUserTracker
+fi
