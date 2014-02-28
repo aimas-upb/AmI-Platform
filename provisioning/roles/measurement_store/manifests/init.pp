8780ce6970f22d32fd66817b3e29ec169242ca05
cosmin-marian <cosmin>
2014-02-28 18:05:13 +0200

    manifest correction

--- provisioning/roles/measurement_store/manifests/init.pp
+++ provisioning/roles/measurement_store/manifests/init.pp
@@ -3,6 +3,6 @@ class measurement_store {
  	class { 'mongodb::globals': 
  		manage_package_repo => true,
  	}
-    class { 'mongodb': : bind_ip => ['0.0.0.0']}
+    class { 'mongodb': bind_ip => ['0.0.0.0']}
 
 }
