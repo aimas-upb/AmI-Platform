ad80f36ddb8eb283896249b8acea93fd4551dcb0
cosmin-marian <cosmin>
2014-02-28 17:44:25 +0200

    manifest correction

--- provisioning/roles/measurement_store/manifests/init.pp
+++ provisioning/roles/measurement_store/manifests/init.pp
@@ -1,7 +1,9 @@
 class measurement_store {
 
- 	class { 'mongodb::globals': manage_package_repo => true }
- 	class { 'mongodb::globals': bind_ip => '0.0.0.0' }
+ 	class { 'mongodb::globals': 
+ 		manage_package_repo => true,
+ 		bind_ip => '0.0.0.0'
+ 	}
     class { 'mongodb':}
 
 }
